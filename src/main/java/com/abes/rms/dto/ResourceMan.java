e3acadb107c8f3eaded54da70eba0913e8c2103b
satya pathak <sp0706003@gmail.com>
2025-05-29 16:42:43 +0530

    Resource manager dto added

---
+++ src/main/java/com/abes/rms/dto/ResourceMan.java
@@ -0,0 +1,11 @@
+package com.abes.rms.dto;
+
+
+@SuppressWarnings("resource")
+public class ResourceMan extends User{
+	
+	public ResourceMan(String id, String pass, String name, String email) {
+		super(id, pass, name, email);
+	}
+
+}

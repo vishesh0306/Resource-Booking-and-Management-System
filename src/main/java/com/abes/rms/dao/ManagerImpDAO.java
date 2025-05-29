418ccb169bf738d43ab4b8782522cee22b2047ea
Devansh Singh <devanshdmp15@gmail.com>
2025-05-29 17:16:20 +0530

    ManagerDAO Added

---
+++ src/main/java/com/abes/rms/dao/ManagerImpDAO.java
@@ -0,0 +1,43 @@
+package rms.dao;
+
+import rms.dto.*; 
+import rms.util.CollectionsUtil;
+
+public class ManagerImpDAO implements ManagerDAO{
+	@Override
+	public boolean addUser(String id, String pass, String name, String email) {
+		for (ResourceMan u : CollectionsUtil.managers) {
+			if (u != null && u.getId().equals(id)) {
+				return false;
+			}
+		}
+		ResourceMan manager = new ResourceMan(id, pass, name, email);;
+		
+		CollectionsUtil.managers.add(manager);
+		return true;
+	}
+	
+	@Override
+	public boolean verifyUser(String userID, String pass) {
+		for(ResourceMan user : CollectionsUtil.managers) {
+			if(user.getId().equalsIgnoreCase(userID) && user.getPass().equalsIgnoreCase(pass)) return true;
+		}
+		return false;
+	}
+	
+	@Override
+	public ResourceMan getUser(String userID, String pass) {
+		for(ResourceMan user : CollectionsUtil.managers) {
+			if(user.getId().equals(userID) && user.getPass().equals(pass)) return user;
+		}
+		return null;
+	}
+	
+	@Override
+	public boolean isManagerPresent(String userID, String pass) {
+		for(ResourceMan manager : CollectionsUtil.managers) {
+			if(manager.getId().equalsIgnoreCase(userID) && manager.getPass().equalsIgnoreCase(pass)) return true;
+		}
+		return false;
+	}
+}

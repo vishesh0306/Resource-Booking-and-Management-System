661ed3e508d2d55edd1addba6e687ca751380b5c
shail-20 <shail.21b0121109@abes.ac.in>
2025-05-30 19:02:22 +0530

    Report and Cart added

---
+++ src/main/java/com/abes/rms/dao/CartDAO.java
@@ -0,0 +1,11 @@
+package com.abes.rms.dao;
+
+import java.util.ArrayList;  
+import com.abes.rms.dto.*;
+
+
+public interface CartDAO {
+	public ArrayList<Booking> getCart(RegularUser user);
+	public ArrayList<Booking> getCartBooking(RegularUser user);
+	public boolean updateRoom(RegularUser user, ArrayList<Booking> temp); 
+}
---
+++ src/main/java/com/abes/rms/services/ReportService.java
@@ -0,0 +1,27 @@
+package com.abes.rms.service;
+
+import com.abes.rms.dao.BookingImpDAO;
+import com.abes.rms.dao.RoomImpDAO;
+import com.abes.rms.dto.Room;
+
+public class ReportService {
+	public Room maxBookedRoom() {
+		int maxi = 0;
+		Room temp = null;
+		for(Room room : new RoomImpDAO().getAllRooms()) {
+			if(room.getCount() > maxi) {
+				maxi = Math.max(maxi, room.getCount());
+				temp = room;
+			}
+		}
+		return temp;
+	}
+	
+	public int totalBooking() {
+		return new BookingImpDAO().getNoOfBookings();
+	}
+	
+	public double revenue() {
+		return new BookingImpDAO().calcRevenue();
+	}
+}

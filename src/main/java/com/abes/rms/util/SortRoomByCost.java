7773d083e039fa5d81a3440982a31fc803ca27b0
anshika1719 <canshika010@gmail.com>
2025-05-30 11:28:20 +0530

    util files- colectionUtil and searching  added

---
+++ src/main/java/com/abes/rms/util/SortRoomByCost.java
@@ -0,0 +1,13 @@
+package com.abes.rms.util;
+
+import java.util.Comparator;
+import com.abes.rms.dto.Room;
+
+public class SortRoomByCost implements Comparator<Room>{
+
+	@Override
+	public int compare(Room o1, Room o2) {
+		return Double.compare(o1.getCostPerHour(), o2.getCostPerHour());  
+	}	
+
+}
